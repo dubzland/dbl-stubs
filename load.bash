860bd18ab33dbffc2d0d3eb088f5e0d3d9142fcb
Josh Williams <jdubz@dubzland.net>
2023-01-22 19:03:50 +0000

    Include assert_stub_with_args on load

--- load.bash
+++ load.bash
@@ -1,3 +1,4 @@
 source "$(dirname "${BASH_SOURCE[0]}")/src/stubs.bash"
 source "$(dirname "${BASH_SOURCE[0]}")/src/assert_stub.bash"
 source "$(dirname "${BASH_SOURCE[0]}")/src/assert_stub_args.bash"
+source "$(dirname "${BASH_SOURCE[0]}")/src/assert_stub_with_args.bash"
